3a03765447ca7f65b2754be21c47bf627907c3ce
McKale Chung <mckale.chung@gmail.com>
2021-12-31 22:09:57 -0700

    IT WORKS LETS GO

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Button } from './button';
 
-
-
 class App extends React.Component{
     constructor(props){
       super(props);
       this.state = {
 
         allInfo: [],
-        Name: [],
+        Name: "Nothing",
         Image: [],
         Number: []
           /* I'm putting all of the key value pairs I deem important into the initial state. 
@@ -28,7 +26,7 @@ class App extends React.Component{
         const response = await fetch('https://enigmatic-beach-52405.herokuapp.com/https://www.fishwatch.gov/api/species');
         const data = await response.json();
         this.setState({ 
-          allInfo: data
+          allInfo: data,
         })
     }
     
@@ -36,21 +34,29 @@ class App extends React.Component{
 
 
     /*Essentially the api information is a massive object split with 700+ different objects containing arrays for each fish species. 
-    I'm just finding a random number in that object length to get the random fish for our user. */
+    I'm just finding a random number in that object length to get the random fish for our user. The returned variable, choice, gives us 
+    all of the information for a certain type of fish. I had a bit of confusion here because I 
+    thought choice was giving us an index rather than a full array value. */
     randomChoice() {
       const newLocal = this.state.allInfo;
       const choice = newLocal[Math.floor(Math.random()*newLocal.length)];
       return choice;
     }
     
+    /*This sets the state for each specific key value pair using the information given by th api. It takes the randomly generated 
+    array object and uses it's key value pairs. */
+    
     handleClick() {
       const newLocal = this.state.allInfo;
       //const index = this.randomChoice;
       this.setState({
-        Name: this.randomChoice["Species Name"],
-       
+        Name: this.randomChoice()["Species Name"],
       })
     }
+
+    
+
+
     
     render() {
         return (
@@ -58,8 +64,9 @@ class App extends React.Component{
                 <h1>Random Fish Info Generator</h1>
                 <Button onClick = {this.handleClick}>
         </Button>
-        <h2>{this.state.Name}</h2>
-        <img src={this.state.Image}></img>
+        <h2 id='name'>hi{this.state.Name}</h2>
+        
+        
             </div>
         )
     }
@@ -68,3 +75,4 @@ class App extends React.Component{
 
   export default App;
   ReactDOM.render(<App />, document.getElementById('app'));
+  //<img id='image'src={this.state.Image}></img>
--- src/button.js
+++ src/button.js
@@ -3,11 +3,11 @@ import React from 'react';
 export class Button extends React.Component {
 	render() {
 		return (
-			<button 
-                onClick = {this.props.onClick}
-				>
+			<button onClick = {this.props.onClick}>
 				Learn now!
 			</button>
 		);
 	}
 }
+
+//Just a simple button using the prop onClick as seen on App.js

13a1c2b44b2adb8761b6b76fb68d96159f197cea
McKale Chung <mckale.chung@gmail.com>
2021-12-31 18:53:41 -0700

    changeing

--- src/App.js
+++ src/App.js
@@ -12,28 +12,40 @@ class App extends React.Component{
         allInfo: [],
         Name: [],
         Image: []
+          /* I'm putting all of the key value pairs I deem important into the initial state. 
+              I'm also putting in all of the info gathered from the api so I can access it throughout the entire file. 
+              I was running into issues earlier with scope, and thought this would work as a solution. */
+
       }
-  
+      this.handleClick = this.handleClick.bind(this); //binding it 
     }
 
     //fetch request for the url
     
     async componentDidMount() {
-        // GET request using fetch with async/await
+        // GET request using fetch with async/await. There is no api key requiered. 
         const response = await fetch('https://enigmatic-beach-52405.herokuapp.com/https://www.fishwatch.gov/api/species');
         const data = await response.json();
-        this.setState({ allInfo: data.total })
+        this.setState({ allInfo: data })
     }
+    
+    
 
+
+    /*Essentially the api information is a massive object split with 700+ different objects containing arrays for each fish species. 
+    I'm just finding a random number in that object length to get the random fish for our user. */
     randomChoice() {
-      const choice = this.state.allInfo[Math.floor(Math.random()*Object.keys(allInfo).length)];
+      const newLocal = this.state.allInfo;
+      const choice = newLocal[Math.floor(Math.random()*Object.keys(newLocal).length)];
       return choice;
     }
     
     handleClick() {
+      const newLocal = this.state.allInfo;
+      //const index = this.randomChoice;
       this.setState({
-        Name: this.state.allInfo[this.randomChoice]["Species Name"],
-        Image: this.state.allInfo[this.randomChoice]["Image Gallaery"][0]
+        Name: newLocal[this.randomChoice()]["Species Name"],
+        Image: newLocal[this.randomChoice()]["Image Gallaery"][0]
       })
     }
     

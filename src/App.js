1889aed19d30b0ae7f69a0594230c7b614b66070
McKale Chung <mckale.chung@gmail.com>
2021-12-31 21:15:29 -0700

    fixed randomizer

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends React.Component{
 
         allInfo: [],
         Name: [],
-        Image: []
+        Image: [],
+        Number: []
           /* I'm putting all of the key value pairs I deem important into the initial state. 
               I'm also putting in all of the info gathered from the api so I can access it throughout the entire file. 
               I was running into issues earlier with scope, and thought this would work as a solution. */
@@ -26,7 +27,9 @@ class App extends React.Component{
         // GET request using fetch with async/await. There is no api key requiered. 
         const response = await fetch('https://enigmatic-beach-52405.herokuapp.com/https://www.fishwatch.gov/api/species');
         const data = await response.json();
-        this.setState({ allInfo: data })
+        this.setState({ 
+          allInfo: data
+        })
     }
     
     
@@ -36,7 +39,7 @@ class App extends React.Component{
     I'm just finding a random number in that object length to get the random fish for our user. */
     randomChoice() {
       const newLocal = this.state.allInfo;
-      const choice = newLocal[Math.floor(Math.random()*Object.keys(newLocal).length)];
+      const choice = newLocal[Math.floor(Math.random()*newLocal.length)];
       return choice;
     }
     
@@ -44,8 +47,8 @@ class App extends React.Component{
       const newLocal = this.state.allInfo;
       //const index = this.randomChoice;
       this.setState({
-        Name: newLocal[this.randomChoice()]["Species Name"],
-        Image: newLocal[this.randomChoice()]["Image Gallaery"][0]
+        Name: this.randomChoice["Species Name"],
+       
       })
     }
     
